438027f5f8c80b298e5ef4576fecc94511141882
Ali <modaresi.mr@gmail.com>
2021-06-24 13:30:22 +0430

    add summary.php

---
+++ admin/summary.php
@@ -0,0 +1,30 @@
+<?php
+include_once('common.php');
+
+$result1 = $mysqli->query("SELECT * FROM Votes order by rand()client_loop: send disconnect: Connection reset
+PS C:\Users\Ali> $result1->num_rows == 0) return myDie("Error: Your secret token is invalid",'danger');
+myheader();
+while ($vote=$result1->fetch_assoc()){
+$voteid=$vote['ID'];
+$result = $mysqli->query("SELECT * from VoteDetails as d where d.VoteID=".$voteid." order by d.Preference" );
+//if (!$result||$result->num_rows == 0) return myDie("Error: No Vote registered.",'danger');
+
+
+?>
+                <div id="list" class="row">
+                        <?php
+                        $first=true;
+                          // output data of each row
+                          while($row = $result->fetch_assoc()) {
+                                if($first){$first=false;}else{echo '>';}
+                                echo mb_chr(64+ $row["CandidateId"]);
+                          }
+
+                        ?>
+</br>
+        </div>
+
+<?php
+}
+myfooter();
+?>
